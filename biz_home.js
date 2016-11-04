c3d943401a21e9f3adcbb31b3ed52825590c2dfc
R5meason <melynda.eason@gmail.com>
2016-11-04 11:08:34 -0700

    added some addt'l styling and comments to biz side

--- biz_home.js
+++ biz_home.js
@@ -10,10 +10,6 @@ firebase.initializeApp(config);
 var fbRef = firebase.database();
 
 
-var uluru = {
-    lat: null,
-    lng: null
-};
 //takes user's current location, and stores it into the uluru object above.
 var startPos;
 navigator.geolocation.getCurrentPosition(geoSuccess);
@@ -21,11 +17,70 @@ function geoSuccess(position) {
     startPos = position;
     uluru.lat = startPos.coords.latitude;
     uluru.lng = startPos.coords.longitude;
-    // console.log(uluru);
 }
 
+//stores location of app user
+var uluru = {
+    lat: null,
+    lng: null
+};
 
-//displays current deals the business has in effect.
+//createAccount function takes the info entered on the sign up page, makes it into an object, and sends it to firebase
+var createAccount = function (name, street, city, state, zip, phone) {
+    var bizObj = {};
+    //takes the parameters passed in to this function, and makes them part of the empty object defined above
+    bizObj.biz_name = name;
+    bizObj.street = street;
+    bizObj.city = city;
+    bizObj.state = state;
+    bizObj.zip = zip;
+    bizObj.phone = phone;
+    bizObj.code = "";
+    bizObj.status = "";
+    //location takes the user's current location and sets it as the business location
+    bizObj.location = {lat: uluru.lat, lng: uluru.lng};
+    //pushing info to firebase
+    fbRef.ref('biz/').push(bizObj);
+    //confirmation screen is displayed after the submit button is clicked, and
+    //displays the entered information back to confirm
+    document.getElementById('sign_up').innerHTML = "Thank You!";
+    document.getElementById('info_confirmed').innerHTML = "Your account has been created: <br> " +
+        bizObj.biz_name
+        + "<br>" +
+        bizObj.street
+        + "<br>" +
+        bizObj.city
+        + " " +
+        bizObj.state
+        + " " +
+        bizObj.zip
+        + "<br>" +
+        bizObj.phone;
+};
+
+//creates new deal & quantity.
+var newDeal = function (deal, qty) {
+    //conditional checks to make sure the qty is a number between 1-10
+    if (qty > 0 && qty < 11) {
+        var dealMsg = {
+            deal
+        };
+        //loop that send the deal to firebase depending on the qty entered, i.e. qty of 2 = deal generated twice;
+        for (var i = 0; i <= qty - 1; i++) {
+            console.log(qty + " " + deal + " deals has been entered");
+            fbRef.ref('biz/_test/deals').push(dealMsg);
+        }
+        //text that displays after the deal has been entered
+        document.getElementById("new_info").innerHTML = "The following deal has been posted:<br> " + deal + " (qty: " + qty + " )";
+        document.getElementById('newDeal').value = "";
+    }
+    else {
+        //text that displays if an invalid qty has been entered
+        document.getElementById("new_info").innerHTML = "Please enter a valid number from 1 to 10";
+    }
+};
+
+//function shows the business its deals currently in effect
 var currentDeal = function () {
     var current = [];
     var query = firebase.database().ref("biz/_test/deals/").orderByKey();
@@ -33,16 +88,16 @@ var currentDeal = function () {
         .then(function (snapshot) {
             snapshot.forEach(function (childSnapshot) {
                 var childData = childSnapshot.val();
-                current.push(childData.deal);
-                current.push("<br>");
+                console.log("deal: " + childData.deal + " / code: " + childSnapshot.key);
+                // var modCode = childSnapshot.key.substring(1);
+                current.push(childData.deal + "<button class='redeemBtn'>Redeem</button><br>");
                 document.getElementById("current_info").innerHTML = "Current Deals:<br> " + current;
-
             });
         });
 };
 
 //updates function reads the data stored in firebase and displays it on screen.
-//Any changes to the business' info are displayed in real-time.
+//Changes to the business' info are displayed in real-time.
 var updates = {};
 var displayData = function () {
     fbRef.ref('biz/_test').on('value', function (snapshot) {
@@ -59,73 +114,26 @@ var displayData = function () {
 
 //updateData takes in two parameters in order to update the appropriate field in firebase with the new info.
 var updateData = function (newInfo, field) {
-    updates['biz/_test/'+field] = newInfo;
+    updates['biz/_test/' + field] = newInfo;
     fbRef.ref().update(updates);
-    document.getElementById('change_confirmed').innerHTML = "Update Confirmed: "+newInfo;
+    document.getElementById('change_confirmed').innerHTML = "Update Confirmed: " + newInfo + "<br>";
     document.getElementById(field).value = "";
 };
 
-var newDeal = function (deal, qty) {
-    if (qty > 0 && qty < 11 ) {
-        var dealMsg = {
-            deal
-        };
-        for (var i = 0; i <= qty -1; i++) {
-            console.log(qty +" "+deal+ " deals has been entered");
-            fbRef.ref('biz/_test/deals').push(dealMsg);
-        }
-        // fbRef.ref('biz/_test/deals').push(dealMsg);
-        //text that displays after the deal has been entered
-        document.getElementById("new_info").innerHTML = "The following deal has been posted:<br> " + deal + " (qty: "+qty+" )";
-        document.getElementById('newDeal').value = "";
-    }
-    else {
-        document.getElementById("new_info").innerHTML = "Please enter a valid number from 1 to 10";
-    }
+var redeem = function(){
+    var current = [];
+    var query = firebase.database().ref("biz/_test/deals/").orderByKey();
+    query.once("value")
+        .then(function (snapshot) {
+            snapshot.forEach(function (childSnapshot) {
+                var childData = childSnapshot.val();
+                console.log("deal: " + childData.deal + " / code: " + childSnapshot.key);
 
+                current.push(childData.deal + "<button class='redeemBtn'>Redeem</button><br>");
+                document.getElementById("current_info").innerHTML = "Current Deals:<br> " + current;
+            });
+        });
 };
-
-//createAccount function takes the info entered on the sign up page, makes it into an object, and sends it to firebase
-var createAccount = function(name, street, city, state, zip, phone) {
-    bizObj ={};
-    //takes the parameters passed in to this function, and makes them part of the empty object defined above
-    bizObj.biz_name = name;
-    bizObj.street = street;
-    bizObj.city = city;
-    bizObj.state= state;
-    bizObj.zip= zip;
-    bizObj.phone= phone;
-    bizObj.code="";
-    bizObj.status="";
-    //location takes the user's current location and sets it as the business location
-    bizObj.location = {lat: uluru.lat, lng: uluru.lng};
-    //pushing info to firebase
-    fbRef.ref('biz/').push(bizObj);
-    //confirmation screen is displayed after the submit button is clicked, and
-    //displays the entered information back to confirm
-    document.getElementById('sign_up').innerHTML = "Thank You!";
-    document.getElementById('info_confirmed').innerHTML = "Your account has been created: <br> "+
-        bizObj.biz_name
-        +"<br>"+
-        bizObj.street
-        +"<br>"+
-        bizObj.city
-        +" "+
-        bizObj.state
-        +" "+
-        bizObj.zip
-        +"<br>"+
-        bizObj.phone;
-    //bizObj is empty after info is sent to firebase
-    bizObj ={};
-};
-
-
-
-
-
-
-
 
 
 
