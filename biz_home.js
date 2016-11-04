0e36eb689d288ff147552a4f4d68901eabeb2f6f
R5meason <melynda.eason@gmail.com>
2016-11-03 18:40:46 -0700

    added deal qty

--- biz_home.js
+++ biz_home.js
@@ -24,6 +24,7 @@ function geoSuccess(position) {
     // console.log(uluru);
 }
 
+
 //displays current deals the business has in effect.
 var currentDeal = function () {
     var current = [];
@@ -34,7 +35,7 @@ var currentDeal = function () {
                 var childData = childSnapshot.val();
                 current.push(childData.deal);
                 current.push("<br>");
-                document.getElementById("current_info").innerText = "Current Deals:<br> " + current;
+                document.getElementById("current_info").innerHTML = "Current Deals:<br> " + current;
 
             });
         });
@@ -46,33 +47,42 @@ var updates = {};
 var displayData = function () {
     fbRef.ref('biz/_test').on('value', function (snapshot) {
         updates = snapshot.val();
-        document.getElementById("bizName").innerText = updates.biz_name;
-        document.getElementById("street_address").innerText = updates.street;
-        document.getElementById("city_name").innerText = updates.city;
-        document.getElementById("state_of").innerText = updates.state;
-        document.getElementById("zip_code").innerText = updates.zip;
-        document.getElementById("phone_num").innerText = updates.phone;
+        document.getElementById("bizName").innerHTML = updates.biz_name;
+        document.getElementById("street_address").innerHTML = updates.street;
+        document.getElementById("city_name").innerHTML = updates.city;
+        document.getElementById("state_of").innerHTML = updates.state;
+        document.getElementById("zip_code").innerHTML = updates.zip;
+        document.getElementById("phone_num").innerHTML = updates.phone;
         updates = {};
     });
 };
 
 //updateData takes in two parameters in order to update the appropriate field in firebase with the new info.
 var updateData = function (newInfo, field) {
-updates['biz/_test/'+field] = newInfo;
-fbRef.ref().update(updates);
-document.getElementById('change_confirmed').innerText = "Update Confirmed: "+newInfo;
-document.getElementById(field).value = "";
+    updates['biz/_test/'+field] = newInfo;
+    fbRef.ref().update(updates);
+    document.getElementById('change_confirmed').innerHTML = "Update Confirmed: "+newInfo;
+    document.getElementById(field).value = "";
 };
 
-//newDeal takes the deal info entered, makes it into and object to store in firebase
-var newDeal = function (deal) {
-    var dealMsg = {
-        deal
-    };
-    fbRef.ref('biz/_test/deals').push(dealMsg);
-    //text that displays after the deal has been entered
-    document.getElementById("new_info").innerText = "The following deal has been posted:<br> " + deal;
-    document.getElementById('newDeal').value = "";
+var newDeal = function (deal, qty) {
+    if (qty > 0 && qty < 11 ) {
+        var dealMsg = {
+            deal
+        };
+        for (var i = 0; i <= qty -1; i++) {
+            console.log(qty +" "+deal+ " deals has been entered");
+            fbRef.ref('biz/_test/deals').push(dealMsg);
+        }
+        // fbRef.ref('biz/_test/deals').push(dealMsg);
+        //text that displays after the deal has been entered
+        document.getElementById("new_info").innerHTML = "The following deal has been posted:<br> " + deal + " (qty: "+qty+" )";
+        document.getElementById('newDeal').value = "";
+    }
+    else {
+        document.getElementById("new_info").innerHTML = "Please enter a valid number from 1 to 10";
+    }
+
 };
 
 //createAccount function takes the info entered on the sign up page, makes it into an object, and sends it to firebase
@@ -93,8 +103,8 @@ var createAccount = function(name, street, city, state, zip, phone) {
     fbRef.ref('biz/').push(bizObj);
     //confirmation screen is displayed after the submit button is clicked, and
     //displays the entered information back to confirm
-    document.getElementById('sign_up').innerText = "Thank You!";
-    document.getElementById('info_confirmed').innerText = "Your account has been created: <br> "+
+    document.getElementById('sign_up').innerHTML = "Thank You!";
+    document.getElementById('info_confirmed').innerHTML = "Your account has been created: <br> "+
         bizObj.biz_name
         +"<br>"+
         bizObj.street
@@ -109,3 +119,46 @@ var createAccount = function(name, street, city, state, zip, phone) {
     //bizObj is empty after info is sent to firebase
     bizObj ={};
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
