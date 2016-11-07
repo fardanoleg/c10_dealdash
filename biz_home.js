e7e9d9585c7ae9a81f4fda480e76fb647239f44f
R5meason <melynda.eason@gmail.com>
2016-11-06 21:07:43 -0800

    added max number of deals allowed

--- biz_home.js
+++ biz_home.js
@@ -26,7 +26,7 @@ var uluru = {
 };
 
 //createAccount function takes the info entered on the sign up page, makes it into an object, and sends it to firebase
-var createAccount = function (name, street, city, state, zip, phone) {
+var createAccount = function (name, street, city, state, zip, phone, email) {
     var bizObj = {};
     //takes the parameters passed in to this function, and makes them part of the empty object defined above
     bizObj.biz_name = name;
@@ -35,6 +35,7 @@ var createAccount = function (name, street, city, state, zip, phone) {
     bizObj.state = state;
     bizObj.zip = zip;
     bizObj.phone = phone;
+    bizObj.email = email;
     bizObj.code = "";
     bizObj.status = "";
     //location takes the user's current location and sets it as the business location
@@ -55,45 +56,79 @@ var createAccount = function (name, street, city, state, zip, phone) {
         + " " +
         bizObj.zip
         + "<br>" +
-        bizObj.phone;
+        bizObj.phone
+        + "<br>" +
+        bizObj.email;
 };
+
+// checks the amount of deals already in effect. Function is called when a new deal is entered.
+var numOfDeals = [];
+var amtOfDealsCheck = function () {
+    var query = firebase.database().ref("biz/_test/deals/").orderByKey();
+    query.once("value")
+        .then(function (snapshot) {
+            snapshot.forEach(function (childSnapshot) {
+                childData = childSnapshot.val();
+                var dealInfo = childData.deal;
+                numOfDeals.push(dealInfo);
+            })
+        })
+};
+
 
 //creates new deal & quantity.
 var newDeal = function (deal, qty) {
-    //conditional checks to make sure the qty is a number between 1-10
-    if (qty > 0 && qty < 11) {
-        var dealMsg = {
-            deal
-        };
-        //loop that send the deal to firebase depending on the qty entered, i.e. qty of 2 = deal generated twice;
-        for (var i = 0; i <= qty - 1; i++) {
-            console.log(qty + " " + deal + " deals has been entered");
-            fbRef.ref('biz/_test/deals').push(dealMsg);
-        }
-        //text that displays after the deal has been entered
-        document.getElementById("new_info").innerHTML = "The following deal has been posted:<br> " + deal + " (qty: " + qty + " )";
-        document.getElementById('newDeal').value = "";
+    //amount of current deals is compared with the max amount of deals allowed (5 deals)
+    amtOfDealsCheck();
+    if (numOfDeals.length >= 5 || (numOfDeals.length + qty) >= 5) {
+        document.getElementById("new_info").innerHTML = "You have exceeded the maximum amount of deals"
     }
     else {
-        //text that displays if an invalid qty has been entered
-        document.getElementById("new_info").innerHTML = "Please enter a valid number from 1 to 10";
+        //conditional checks to make sure the qty is a number between 1-10
+        if (qty > 0 && qty < 6) {
+            var dealMsg = {
+                deal
+            };
+            //loop that send the deal to firebase depending on the qty entered, i.e. qty of 2 = deal generated twice;
+            for (var i = 0; i <= qty - 1; i++) {
+                console.log(qty + " " + deal + " deals has been entered");
+                fbRef.ref('biz/_test/deals').push(dealMsg);
+            }
+            //text that displays after the deal has been entered
+            document.getElementById("new_info").innerHTML = "The following deal has been posted:<br> " + deal + " (qty: " + qty + " )";
+            document.getElementById('newDeal').value = "";
+            document.getElementById('newDealQty').value = "";
+        }
+        else {
+            //text that displays if an invalid qty has been entered
+            document.getElementById("new_info").innerHTML = "Please enter a valid number from 1 to 10";
+        }
     }
 };
 
-//function shows the business its deals currently in effect
+// //function shows the business its deals currently in effect & provides a customer code
 var currentDeal = function () {
     var current = [];
     var query = firebase.database().ref("biz/_test/deals/").orderByKey();
     query.once("value")
         .then(function (snapshot) {
             snapshot.forEach(function (childSnapshot) {
-                var childData = childSnapshot.val();
-                console.log("deal: " + childData.deal + " / code: " + childSnapshot.key);
-                // var modCode = childSnapshot.key.substring(1);
-                current.push(childData.deal + "<button class='redeemBtn'>Redeem</button><br>");
+                childData = childSnapshot.val();
+                var dealInfo = childData.deal;
+                var redemptionCode = childSnapshot.key;
+                console.log("Deal info: " + dealInfo + " Redemption code: " + redemptionCode);
+                current.push(dealInfo);
+                current.push("<button id='redeemBtn' onclick='displayCustCode(" + "\" " + redemptionCode + "\" " + ")'>redeem</button> <br>");
                 document.getElementById("current_info").innerHTML = "Current Deals:<br> " + current;
             });
         });
+};
+
+//displays the shortened code, created from the randomly generated key firebase assigns to it.
+var displayCustCode = function (code) {
+    console.log(code);
+    var shortCode = code.substring(16);
+    document.getElementById("redeem_screen").innerHTML = "<br> Customer Code: " + shortCode + "<br>";
 };
 
 //updates function reads the data stored in firebase and displays it on screen.
@@ -108,6 +143,7 @@ var displayData = function () {
         document.getElementById("state_of").innerHTML = updates.state;
         document.getElementById("zip_code").innerHTML = updates.zip;
         document.getElementById("phone_num").innerHTML = updates.phone;
+        document.getElementById("email_addy").innerHTML = updates.email;
         updates = {};
     });
 };
@@ -119,6 +155,14 @@ var updateData = function (newInfo, field) {
     document.getElementById('change_confirmed').innerHTML = "Update Confirmed: " + newInfo + "<br>";
     document.getElementById(field).value = "";
 };
+
+
+
+
+
+
+
+
 
 
 
